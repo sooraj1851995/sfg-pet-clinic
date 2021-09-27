39808642746aec84aca63fd1b0ef4431eda84b4b
sooraj1851995 <sooraj1851995@gmail.com>
2021-09-27 18:41:53 +0530

    Refactor service interfaces to common base interface. Closes #6

--- pet-clinic-data/src/main/java/elixr/springframework/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/elixr/springframework/sfgpetclinic/services/VetService.java
@@ -2,12 +2,7 @@ package elixr.springframework.sfgpetclinic.services;
 
 import elixr.springframework.sfgpetclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
+public interface VetService extends CrudService<Vet, Long>{
 
     Vet findByLastName(String lastName);
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
 }
