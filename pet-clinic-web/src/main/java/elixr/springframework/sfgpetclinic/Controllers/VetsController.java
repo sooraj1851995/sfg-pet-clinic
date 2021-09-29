137c241bd048bdfe74ea3eab37d96d98428b073d
sooraj1851995 <sooraj1851995@gmail.com>
2021-09-29 12:44:18 +0530

    Added Vets index page and Controller. Closes #9

---
+++ pet-clinic-web/src/main/java/elixr/springframework/sfgpetclinic/Controllers/VetsController.java
@@ -0,0 +1,14 @@
+package elixr.springframework.sfgpetclinic.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetsController {
+
+    @RequestMapping({"/vets","vets/index","vets/index.html"})
+    public String listVets(){
+
+        return "vets/index";
+    }
+}
