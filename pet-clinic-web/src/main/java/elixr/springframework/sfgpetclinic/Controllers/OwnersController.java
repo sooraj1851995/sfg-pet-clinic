2ae7ff22a389b191b60fea38be2ff78a19b395e1
sooraj1851995 <sooraj1851995@gmail.com>
2021-10-02 16:58:46 +0530

    Listed owners in index page. Closes#13

--- pet-clinic-web/src/main/java/elixr/springframework/sfgpetclinic/Controllers/OwnersController.java
+++ pet-clinic-web/src/main/java/elixr/springframework/sfgpetclinic/Controllers/OwnersController.java
@@ -1,14 +1,22 @@
 package elixr.springframework.sfgpetclinic.Controllers;
 
+import elixr.springframework.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnersController {
+    private final OwnerService ownerService;
+
+    public OwnersController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
 
     @RequestMapping({"","/","/index","/index.html"})
-    public String listOwners(){
+    public String listOwners(Model model){
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
