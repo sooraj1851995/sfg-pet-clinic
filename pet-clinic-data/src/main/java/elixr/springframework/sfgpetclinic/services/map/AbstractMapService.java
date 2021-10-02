d94680b1182b4ccb0a401dd5707cfe0a51b7be60
sooraj1851995 <sooraj1851995@gmail.com>
2021-10-02 18:21:23 +0530

    Updated map impl to  manage setting of ID property. Closes#15

--- pet-clinic-data/src/main/java/elixr/springframework/sfgpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/elixr/springframework/sfgpetclinic/services/map/AbstractMapService.java
@@ -1,12 +1,11 @@
 package elixr.springframework.sfgpetclinic.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import elixr.springframework.sfgpetclinic.model.BaseEntity;
 
-public abstract class AbstractMapService<T, ID> {
-    protected Map<ID, T> map= new HashMap<>();
+import java.util.*;
+
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long > {
+    protected Map<Long, T> map= new HashMap<>();
 
     Set<T> findAll(){
         return new HashSet<>(map.values());
@@ -15,9 +14,19 @@ public abstract class AbstractMapService<T, ID> {
         return map.get(id);
     }
 
-    T save(ID id, T object){
+    T save(T object){
+        if (object != null){
 
-        map.put(id,object);
+            if(object.getId() == null){
+
+                object.setId(getNextId());
+            }
+
+            map.put(object.getId(), object);
+        }
+        else {
+            throw new RuntimeException("object canot be null");
+        }
         return object;
 
     }
@@ -30,6 +39,18 @@ public abstract class AbstractMapService<T, ID> {
         map.remove(id);
     }
 
+    private long getNextId(){
 
+        Long nextId = null;
+
+        try{
+
+            nextId = Collections.max(map.keySet()) + 1;
+        }
+        catch (NoSuchElementException e){
+            nextId = 1L;
+        }
+        return nextId;
+    }
 
 }
--- pet-clinic-web/src/main/java/elixr/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/elixr/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -23,13 +23,11 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Ethun");
         owner1.setLastName("Hunt");
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Julie");
         owner2.setLastName("Hunt");
         ownerService.save(owner2);
@@ -37,13 +35,11 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners!!!!!!!!!!!!");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Benchy");
         vet1.setLastName("Michale");
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Luther");
         vet2.setLastName("John");
         vetService.save(vet2);
