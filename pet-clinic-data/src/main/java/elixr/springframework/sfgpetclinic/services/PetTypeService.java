dfe05c2796db2c65c8c428c93cb8848420b709f3
sooraj1851995 <sooraj1851995@gmail.com>
2021-10-09 16:39:29 +0530

    Added PetType Service and Impl. Closes #26

---
+++ pet-clinic-data/src/main/java/elixr/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package elixr.springframework.sfgpetclinic.services;
+
+import elixr.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+}
